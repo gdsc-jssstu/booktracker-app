d0f804357157299646e886e986cad65398e024bd
Manojkc15 <manojkc9845@gmail.com>
2021-10-11 22:07:35 +0530

    Implemented UI as mentioned in README.md

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -17,7 +17,9 @@ class TodoListScreen extends StatefulWidget {
 
 class _TodoListScreenState extends State<TodoListScreen> {
   Future<List<Task>>? _taskList;
-  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
+  // final DateFormat _dateFormatter = DateFormat('MM-dd-yyyy');
+  final DateFormat formatter = DateFormat('d');
+  String day = "";
 
   @override
   void initState() {
@@ -32,44 +34,106 @@ class _TodoListScreenState extends State<TodoListScreen> {
   }
 
   Widget _buildTask(Task task) {
+    void _awaitForDay(BuildContext context) async {
+      final result = await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) =>
+              AddTaskScreen(updateTaskList: _updateTaskList, task: task),
+        ),
+      );
+      setState(() {
+        day = result;
+        // print(index);
+      });
+    }
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25),
         child: Column(
           children: [
-            ListTile(
-              title: Text(
-                task.title!,
-                style: TextStyle(
-                    fontSize: 18,
-                    decoration: task.status == 0
-                        ? TextDecoration.none
-                        : TextDecoration.lineThrough),
+            // ListTile(
+            //   title: Text(
+            //     task.title!,
+            //     style: TextStyle(
+            //         fontSize: 18,
+            //         decoration: task.status == 0
+            //             ? TextDecoration.none
+            //             : TextDecoration.lineThrough),
+            //   ),
+            //   subtitle: Text(
+            //     '${_dateFormatter.format(task.date!)} * ${task.priority}',
+            //     style: TextStyle(
+            //         fontSize: 15,
+            //         decoration: task.status == 0
+            //             ? TextDecoration.none
+            //             : TextDecoration.lineThrough),
+            //   ),
+            //   trailing: Checkbox(
+            //     onChanged: (value) {
+            //       task.status = value! ? 1 : 0;
+            //       DatabaseHelper.instance.updateTask(task);
+            //       _updateTaskList();
+            //     },
+            //     activeColor: Theme.of(context).primaryColor,
+            //     value: task.status == 1 ? true : false,
+            //   ),
+            //   onTap: () => Navigator.push(
+            //       context,
+            //       MaterialPageRoute(
+            //           builder: (_) => AddTaskScreen(
+            //               updateTaskList: _updateTaskList, task: task))),
+            // ),
+            const Divider(),
+            GestureDetector(
+              onTap: () => _awaitForDay(context),
+              child: Container(
+                padding: const EdgeInsets.all(5.0),
+                height: 100.0,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          task.title!,
+                          style: const TextStyle(fontSize: 25.0),
+                        ),
+                      ),
+                    ),
+                    const VerticalDivider(
+                      thickness: 2.0,
+                      color: Colors.black54,
+                    ),
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        const Icon(
+                          Icons.calendar_today,
+                          size: 90.0,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 15.0),
+                          child: Text(
+                            day.isEmpty ? formatter.format(task.date!) : day,
+                            // formatter.format(task.date!),
+                            // day,
+                            style: const TextStyle(fontSize: 30.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              subtitle: Text(
-                '${_dateFormatter.format(task.date!)} * ${task.priority}',
-                style: TextStyle(
-                    fontSize: 15,
-                    decoration: task.status == 0
-                        ? TextDecoration.none
-                        : TextDecoration.lineThrough),
-              ),
-              trailing: Checkbox(
-                onChanged: (value) {
-                  task.status = value! ? 1 : 0;
-                  DatabaseHelper.instance.updateTask(task);
-                  _updateTaskList();
-                },
-                activeColor: Theme.of(context).primaryColor,
-                value: task.status == 1 ? true : false,
-              ),
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => AddTaskScreen(
-                          updateTaskList: _updateTaskList, task: task))),
             ),
-            const Divider()
           ],
         ),
       ),
@@ -82,7 +146,9 @@ class _TodoListScreenState extends State<TodoListScreen> {
 
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.greenAccent,
         appBar: AppBar(
+          backgroundColor: Colors.greenAccent,
           title: const Center(child: Text('Book Tracker')),
           leading: const SizedBox(
             width: 15,
@@ -102,7 +168,8 @@ class _TodoListScreenState extends State<TodoListScreen> {
           ],
         ),
         floatingActionButton: FloatingActionButton(
-          backgroundColor: Theme.of(context).primaryColor,
+          // backgroundColor: Theme.of(context).primaryColor,
+          backgroundColor: Colors.white,
           onPressed: () => {
             Navigator.push(
               context,
@@ -111,11 +178,18 @@ class _TodoListScreenState extends State<TodoListScreen> {
               ),
             )
           },
-          child: const Icon(
+          // child: const Icon(
+          //   Icons.add,
+          //   color: Colors.white,
+          // ),
+          child: Icon(
             Icons.add,
-            color: Colors.white,
+            color: Colors.green[900],
+            size: 35.0,
           ),
         ),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.miniCenterFloat,
         body: FutureBuilder(
           future: _taskList,
           builder: (context, snapshot) {
@@ -125,39 +199,56 @@ class _TodoListScreenState extends State<TodoListScreen> {
               );
             }
 
-            final int? completedTaskCount = (snapshot.data as List<Task>)
-                .where((Task task) => task.status == 1)
-                .toList()
-                .length;
+            // final int? completedTaskCount = (snapshot.data as List<Task>)
+            //     .where((Task task) => task.status == 1)
+            //     .toList()
+            //     .length;
 
             return ListView.builder(
-              padding: const EdgeInsets.symmetric(vertical: 60.0),
+              // padding: const EdgeInsets.symmetric(vertical: 60.0),
               itemCount: 1 + (snapshot.data as List<Task>).length,
               itemBuilder: (BuildContext context, int index) {
                 if (index == 0) {
                   return Padding(
                     padding: const EdgeInsets.symmetric(
-                        vertical: 20.0, horizontal: 40.0),
+                        vertical: 10.0, horizontal: 25.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text(
-                          'Books to be Returned',
-                          style: TextStyle(
-                              color: Color(0xFF018786),
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold),
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.all(15.0),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.rectangle,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20.0)),
+                          ),
+                          child: const Text(
+                            'Hi There 👋,',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        Text(
-                          '$completedTaskCount of ${(snapshot.data as List<Task>).length}',
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w600),
-                        )
+                        // const Text(
+                        //   'Books to be Returned',
+                        //   style: TextStyle(
+                        //       color: Color(0xFF018786),
+                        //       fontSize: 32,
+                        //       fontWeight: FontWeight.bold),
+                        // ),
+                        // const SizedBox(
+                        //   height: 10.0,
+                        // ),
+                        // Text(
+                        //   '$completedTaskCount of ${(snapshot.data as List<Task>).length}',
+                        //   style: const TextStyle(
+                        //       color: Colors.grey,
+                        //       fontSize: 15.0,
+                        //       fontWeight: FontWeight.w600),
+                        // )
                       ],
                     ),
                   );
